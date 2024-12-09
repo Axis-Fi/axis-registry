d0628efd4a66d33d9e7553fce96b602e2933b2a4
Oighty <oighty6@gmail.com>
2024-12-09 11:26:59 -0600

    feat: add signature verifcation and ownable

--- src/MetadataRegistry.sol
+++ src/MetadataRegistry.sol
@@ -1,9 +1,24 @@
 // SPDX-License-Identifier: TBD
 pragma solidity 0.8.19;
 
+import {Ownable} from "@openzeppelin/access/Ownable.sol";
+import {SignatureChecker} from "@openzeppelin/utils/cryptography/SignatureChecker.sol";
 
-// TODO make ownable
-contract AxisMetadataRegistry {
+import {IAuctionHouse} from "@axis-core/interfaces/IAuctionHouse.sol";
+
+contract AxisMetadataRegistry is Ownable {
+    // ========== ERRORS ========== //
+
+    error AlreadyAssigned();
+    error InvalidParam(string param);
+    error InvalidSignature();
+    error NotAuthorized();
+
+    // ========== EVENTS ========== //
+
+    event AuctionRegistered(address auctionHouse, uint96 lotId, string ipfsCID);
+    event CuratorRegistered(address curator, uint256 xId, string ipfsCID);
+    event CuratorUpdated(uint256 xId, string ipfsCID);
 
     // ========== DATA STRUCTURES ========== //
 
@@ -18,12 +33,20 @@ contract AxisMetadataRegistry {
         uint96 lotId;
         string ipfsCID;
     }
-    
-    
+
     // ========== STATE VARIABLES ========== //
 
+    // Typed Data Variables
+    uint256 public chainId;
+    bytes32 internal domainSeparator;
+
+    bytes32 internal constant CURATOR_REGISTRATION_TYPEHASH =
+        keccak256("CuratorRegistration(address curator,uint256 xId,string ipfsCID)");
+
+    /// @notice Address that signs registration payloads after being verified by an offchain service
     address public serviceSigner;
 
+    mapping(address => bool) public isAuctionHouse;
     mapping(address curator => uint256 xId) public curatorId;
     mapping(uint256 xId => string ipfsCID) public curatorMetadata;
 
@@ -31,7 +54,7 @@ contract AxisMetadataRegistry {
 
     // ========== CONSTRUCTOR ========== //
 
-    constructor(address serviceSigner_, address[] calldata auctionHouses_) {
+    constructor(address serviceSigner_, address[] memory auctionHouses_) {
         for (uint256 i = 0; i < auctionHouses_.length; i++) {
             isAuctionHouse[auctionHouses_[i]] = true;
         }
@@ -45,19 +68,19 @@ contract AxisMetadataRegistry {
 
     function registerCurator(CuratorRegistration calldata payload_, bytes calldata signature_) external {
         // Validate the sender is the curator listed in the payload
-        if (msg.sender != payload_.curator) NotAuthorized();
+        if (msg.sender != payload_.curator) revert NotAuthorized();
 
         // Validate the xId is not zero
-        if (payload_.xId == 0) InvalidParam("payload.xId");
+        if (payload_.xId == 0) revert InvalidParam("payload.xId");
 
         // Validate that the curator address is not already assigned an xId
-        if (curatorId[payload_.curator] != 0) AlreadyAssigned();
+        if (curatorId[payload_.curator] != 0) revert AlreadyAssigned();
 
         // Validate that the ipfsCID string is not empty
-        if (bytes(payload_.ipfsCID).length == 0) InvalidParam("payload.ipfsCID");
+        if (bytes(payload_.ipfsCID).length == 0) revert InvalidParam("payload.ipfsCID");
 
         // Validate the service signer signed the payload
-        // TODO add signature validation library
+        if (!isValidSignature(serviceSigner, payload_, signature_)) revert InvalidSignature();
 
         // Store the curator's xId
         curatorId[payload_.curator] = payload_.xId;
@@ -69,7 +92,8 @@ contract AxisMetadataRegistry {
         emit CuratorRegistered(payload_.curator, payload_.xId, payload_.ipfsCID);
     }
 
-    function registerAuction(address auctionHouse_, uint96 lotId_, string ipfsCID_) external {
+    // This function allows an auction to be registered multiple times, which allows updating the data
+    function registerAuction(address auctionHouse_, uint96 lotId_, string calldata ipfsCID_) external {
         // Validate the auction house is supported
         if (!isAuctionHouse[auctionHouse_]) revert InvalidParam("auctionHouse");
         IAuctionHouse auctionHouse = IAuctionHouse(auctionHouse_);
@@ -78,7 +102,7 @@ contract AxisMetadataRegistry {
         if (lotId_ >= auctionHouse.lotCounter()) revert InvalidParam("lotId");
 
         // Validate the caller is the seller of the lot
-        (address seller, ,,,,,,,) = auctionHouse.lotRouting();
+        (address seller,,,,,,,,) = auctionHouse.lotRouting(lotId_);
         if (msg.sender != seller) revert NotAuthorized();
 
         // Validate the ipfsCID string is not empty
@@ -93,7 +117,7 @@ contract AxisMetadataRegistry {
 
     // ========== UPDATE ========== //
 
-    function updateCurator(uint256 xId_, string ipfsCID_) external {
+    function updateCurator(uint256 xId_, string calldata ipfsCID_) external {
         // Validate the xId is not zero
         if (xId_ == 0) revert InvalidParam("xId");
 
@@ -122,9 +146,6 @@ contract AxisMetadataRegistry {
 
         // Store the curator's xId
         curatorId[curator_] = xId_;
-
-        // Emit event
-        emit CuratorAddressAdded(xId_, curator_);
     }
 
     function removeCuratorAddress(uint256 xId_, address curator_) external {
@@ -139,14 +160,11 @@ contract AxisMetadataRegistry {
 
         // Remove the curator's xId
         delete curatorId[curator_];
-
-        // Emit event
-        emit CuratorAddressRemoved(xId_, curator_);
     }
 
     // ========== ADMIN ========== //
 
-    function forceRegisterCurator(address curator_, uint256 xId_, string ipfsCID_) external onlyOwner {
+    function forceRegisterCurator(address curator_, uint256 xId_, string calldata ipfsCID_) external onlyOwner {
         // Validate the xId is not zero
         if (xId_ == 0) revert InvalidParam("xId");
 
@@ -175,9 +193,6 @@ contract AxisMetadataRegistry {
 
         // Store the curator's xId
         curatorId[curator_] = xId_;
-
-        // Emit event
-        emit CuratorAddressAdded(xId_, curator_);
     }
 
     function forceRemoveCuratorAddress(uint256 xId_, address curator_) external onlyOwner {
@@ -189,14 +204,62 @@ contract AxisMetadataRegistry {
 
         // Remove the curator's xId
         delete curatorId[curator_];
-
-        // Emit event
-        emit CuratorAddressRemoved(xId_, curator_);
     }
 
     function updateServiceSigner(address serviceSigner_) external onlyOwner {
         serviceSigner = serviceSigner_;
     }
 
+    // ========== SIGNATURE VALIDATION ========== //
 
+    function isValidSignature(address signer_, CuratorRegistration calldata payload_, bytes calldata signature_)
+        public
+        view
+        returns (bool)
+    {
+        return SignatureChecker.isValidSignatureNow(signer_, getDigest(payload_), signature_);
+    }
+
+    function getDigest(CuratorRegistration calldata payload_) public view returns (bytes32) {
+        return keccak256(
+            abi.encodePacked(
+                hex"1901",
+                DOMAIN_SEPARATOR(),
+                keccak256(
+                    abi.encode(
+                        CURATOR_REGISTRATION_TYPEHASH,
+                        payload_.curator,
+                        payload_.xId,
+                        keccak256(bytes(payload_.ipfsCID))
+                    )
+                )
+            )
+        );
+    }
+
+    /* ========== DOMAIN SEPARATOR ========== */
+
+    function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
+        return block.chainid == chainId ? domainSeparator : computeDomainSeparator();
+    }
+
+    function computeDomainSeparator() internal view virtual returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256("Axis Metadata Registry"),
+                keccak256("v1.0.0"),
+                block.chainid,
+                address(this)
+            )
+        );
+    }
+
+    function updateDomainSeparator() external {
+        require(block.chainid != chainId, "DOMAIN_SEPARATOR_ALREADY_UPDATED");
+
+        chainId = block.chainid;
+
+        domainSeparator = computeDomainSeparator();
+    }
 }
