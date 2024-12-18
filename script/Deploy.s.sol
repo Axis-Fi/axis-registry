4790b0cd73972fb669d794c22f2df680ef224026
Oighty <oighty6@gmail.com>
2024-12-18 09:27:19 -0600

    script: add basic deploy script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+
+import {Script} from "@forge-std/Script.sol";
+import {console2 as console} from "@forge-std/console2.sol";
+import {AxisMetadataRegistry} from "../src/MetadataRegistry.sol";
+
+contract Deploy is Script {
+
+    function deploy() public {
+
+        // Set constructor parameters
+        address serviceSigner = msg.sender;
+        address[] memory auctionHouses = new address[](1);
+        auctionHouses[0] = 0xBA0000c28179CE533233a943d432eddD154E62A3; // base-sepolia batch auction house
+
+        console.log("Deploying AxisMetadataRegistry...");
+        console.log("Service Signer:", serviceSigner);
+        uint256 len = auctionHouses.length;
+        console.log("Number of auctionHouses:", len);
+        for (uint256 i = 0; i < len; i++) {
+            console.log(string.concat("Auction House ", vm.toString(i), ": "), auctionHouses[i]);
+        }
+
+        // Deploy the metadata registry
+        vm.broadcast();
+        AxisMetadataRegistry metadataRegistry = new AxisMetadataRegistry(
+            serviceSigner,
+            auctionHouses
+        );
+
+        console.log("AxisMetadataRegistry deployed at:", address(metadataRegistry));
+    }
+
+}
