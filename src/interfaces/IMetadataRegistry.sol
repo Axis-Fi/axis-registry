0ff2d4ca25500083126412c66719a590a2aa53ee
Jem <0x0xjem@gmail.com>
2025-02-18 17:40:46 +0400

    Interface licence

--- src/interfaces/IMetadataRegistry.sol
+++ src/interfaces/IMetadataRegistry.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: TBD
-pragma solidity ^0.8.19;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
 
 interface IMetadataRegistry {
     // ========== ERRORS ========== //
