5429b3059fde60952051f6344d8181db2e0526b1
Jem <0x0xjem@gmail.com>
2025-02-18 17:39:13 +0400

    Add dummy test to avoid errors

---
+++ test/Dummy.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.19;
+
+import {Test} from "@forge-std/Test.sol";
+
+contract DummyTest is Test {
+    function test_dummy() public view {
+        // Do nothing
+    }
+}
